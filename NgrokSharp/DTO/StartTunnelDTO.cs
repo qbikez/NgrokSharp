6a7de244d3f98c94f2246481b96241f2c598d92f
jakub.pawlowski <jakub.pawlowski@guestline.com>
2023-10-21 20:19:55 +0100

    v3 doesn't have bind_tls

--- NgrokSharp/DTO/StartTunnelDTO.cs
+++ NgrokSharp/DTO/StartTunnelDTO.cs
@@ -6,7 +6,7 @@ namespace NgrokSharp.DTO
         public string addr { get; set; }
         public string proto { get; set; }
         public string name { get; set; }
-        public string bind_tls { get; set; }
+        //public string bind_tls { get; set; }
         public string subdomain { get; set; }
         public string hostname { get; set; }
     }
